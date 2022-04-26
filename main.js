355542f9a15dd5d0a08d39478aeb994bc21205d9
Adrian Gioia <afgioia@gmail.com>
2022-04-26 10:30:48 -0700

    Break for outcaomes

--- main.js
+++ main.js
@@ -2,21 +2,39 @@ const openViewer = function() {
     console.log();
 }
 
+    $('#box-1').on('click', function() {
+        $('.box').toggleClass('.dimmed');
+        $(".viewer-screen > p").text('');
+        $("#box-6").toggleClass("viewer-screen");
+        $(".viewer-screen > p").text("Awesomely sweet button# 1 content activated!");
+    })
+    $("#box-2").on('click', function() {
+        $('.box').toggleClass('.dimmed');
+        $(".viewer-screen > p").text('');
+        $("#box-6").toggleClass("viewer-screen");
+        $(".viewer-screen > p").text("Awesomely sweet button# 2 content activated!");
 
-$(document).ready(() => {
-    $("#box-1").on('click', function(event) {
-        $("#box-6").toggleClass("box-1");
     })
-    $("#box-2").on('click', function(event) {
-        $("#box-6").toggleClass("box-2");
+    $("#box-3").on('click', function() {
+        $('.box').toggleClass('.dimmed');
+        $(".viewer-screen > p").text('');
+        $("#box-6").toggleClass("viewer-screen");
+        $(".viewer-screen > p").text("Awesomely sweet button# 3 content activated!");
     })
-    $("#box-3").on('click', function(event) {
-        $("#box-6").toggleClass("box-3");
+    $("#box-4").on('click', function() {
+        $('.box').toggleClass('.dimmed');
+        $(".viewer-screen > p").text('');
+        $("#box-6").toggleClass("viewer-screen");
+        $(".viewer-screen > p").text("Awesomely sweet button# 4 content activated!");
     })
-    $("#box-4").on('click', function(event) {
-        $("#box-6").toggleClass("box-4");
+    $("#box-5").on('click', function() {
+        $('.box').toggleClass('.dimmed');
+        $(".viewer-screen > p").text('');
+        $("#box-6").toggleClass("viewer-screen");
+        $(".viewer-screen > p").text("Awesomely sweet button# 5 content activated!");
     })
-    $("#box-5").on('click', function(event) {
-        $("#box-6").toggleClass("box-5");
+
+    $('#box-1').on('hover', function(event) {
+        (event.target).text('About');
     })
-});
+

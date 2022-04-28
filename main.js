fd844fc9b9858cc4d4b17ed72aca8a74510fadbf
Adrian Gioia <afgioia@gmail.com>
2022-04-28 09:59:39 -0700

    Made adjustments to all three media query displays.

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 //All functions declared here in the header
-const link = 'https://docs.google.com/spreadsheets/d/11RXPA6fh2GQCeni-7yy4OqrKXia3HE1ScoOFRFssPZk/edit#gid=0"'
+// const link = 'https://docs.google.com/spreadsheets/d/11RXPA6fh2GQCeni-7yy4OqrKXia3HE1ScoOFRFssPZk/edit#gid=0"'
 
-const key = 'AIzaSyD5gvCmsvh1nSoAND0u0MN5FO-e-gdrHtk'
+// const key = 'AIzaSyD5gvCmsvh1nSoAND0u0MN5FO-e-gdrHtk'
 
-const id = "11RXPA6fh2GQCeni-7yy4OqrKXia3HE1ScoOFRFssPZk";
+// const id = "11RXPA6fh2GQCeni-7yy4OqrKXia3HE1ScoOFRFssPZk";
 
-let url = `https://sheets.googleapis.com/v4/spreadsheets/11RXPA6fh2GQCeni-7yy4OqrKXia3HE1ScoOFRFssPZk/values/A1:B1?key=${key}`
+// let url = `https://sheets.googleapis.com/v4/spreadsheets/11RXPA6fh2GQCeni-7yy4OqrKXia3HE1ScoOFRFssPZk/values/A1:B1?key=${key}`
 
-const getContent = function() {
-$.ajax({
-    method: "get",
-    url: url
-})
-.then(function(data) {
-    return data.values[0];
-})
-.catch(function() {
-    console.log("nope");
-})
-}
+// const getContent = function() {
+// $.ajax({
+//     method: "get",
+//     url: url
+// })
+// .then(function(data) {
+//     return data.values[0];
+// })
+// .catch(function() {
+//     console.log("nope");
+// })
+// }
 
 //Removes all .pixel children from .snow-screen div and generates a grayscale pixelated screen
 const snowScreen = function() {
@@ -39,15 +39,14 @@ const generatePixel = function() {
 
 const static = function() {
     setInterval(snowScreen, 200);
-    
 }
 
 const about = function() {
-    const aboutData = getContent();
-    const aboutText = aboutData[1];
+    // const aboutData = getContent();
+    // const aboutText = aboutData[1];
     $("div #viewer-screen.about").append("<p>");
     $("div #viewer-screen.about p").css('grid-column-area')
-    $("div #viewer-screen.about p").text(`${aboutText}`);
+    $("div #viewer-screen.about p").text("I wrote my first program on a TI-82 graphing calculator and have been fascinated by programming languages and design ever since. Collaborative, driven, and comitted to doing great work. I'm a life-long learner in search of a great organization. I'd love to talk to you!");
 }
 
 const languages = function() {
@@ -59,7 +58,10 @@ const languages = function() {
 }
 
 const portfolio = function() {
-    // $("#viewer-screen.portfolio div #project-1").
+    $("div #viewer-screen.portfolio").append("<div><h3>GA SEI Project-1</h3></div>");
+    $("div #viewer-screen.portfolio").append("<div><h3>GA SEI Project-2</h3></div>");
+    $("div #viewer-screen.portfolio").append("<div><h3>GA SEI Project-3</h3></div>");
+    $("div #viewer-screen.portfolio").append("<div><h3>GA SEI Project-4</h3></div>");
 }
 
 const accolades = function() {
